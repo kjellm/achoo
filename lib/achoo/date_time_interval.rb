c532bad14f48578a31b7c2808229f32659982866
Kjell-Magne Øierud <kjellm@acm.org>
2010-02-13 21:58:53 +0100

    More readable output from to_s + some minor refactoring

--- lib/achoo/date_time_interval.rb
+++ lib/achoo/date_time_interval.rb
@@ -1,9 +1,20 @@
 require 'parsedate'
 
+class Achoo; end
+
 class Achoo::DateTimeInterval
+
+  SECONDS_IN_DAY    = 86400
+  SECONDS_IN_HOUR   = 3600
+  SECONDS_IN_MINUTE = 60
 
   attr :start
   attr :end
+
+  def initialize(start_dt=nil, end_dt=nil)
+    self.start = start_dt
+    self.end   = end_dt
+  end
 
   def start=(date_time)
     date_time = Time.local(*ParseDate.parsedate(date_time)) if date_time.kind_of?(String)
@@ -16,15 +27,10 @@ class Achoo::DateTimeInterval
   end
 
   def to_s
-    delta = @end - @start
+    duration = duration_string
+    from_to  = from_to_string
 
-    d = delta.to_i / (86400)
-    delta = delta - d*(86400)
-    h = delta.to_i / (3600)
-    delta = delta - h*(3600)
-    m = delta.to_i / (60)
-
-    sprintf "%s - %s (%d+%02d:%02d)", start, self.end, d, h, m
+    sprintf("(%s) %s", duration, from_to)
   end
 
   def contains(date)
@@ -41,6 +47,46 @@ class Achoo::DateTimeInterval
       && (self.end <=> dt_interval.end) >= 0
     #puts "=== #{test}"
     return test
+  end
+
+  private 
+  
+  def duration_string
+    delta = @end - @start
+    d     = delta.to_i / SECONDS_IN_DAY
+
+    delta = delta - d*SECONDS_IN_DAY
+    h     = delta.to_i / SECONDS_IN_HOUR
+
+    delta = delta - h*SECONDS_IN_HOUR
+    m     = delta.to_i / SECONDS_IN_MINUTE
+
+    sprintf "%d+%02d:%02d", d, h, m
+  end
+
+  def from_to_string
+    # FIX 
+    #  - Don't print month if same as today's month
+    #  - Don't print year if same as today's year
+    #  - Etc
+
+    from = nil
+    if start.send(:to_date) == Date.today
+      from = start.strftime("Today %R")
+    else
+      from = start.strftime("%a %e. %b %Y %R")
+    end
+    
+    to = nil
+    if self.end.send(:to_date) == start.send(:to_date)
+      to = self.end.strftime("%R")
+    elsif self.end.send(:to_date) == Date.today
+      to = self.end.strftime("Today %R")
+    else
+      to = self.end.strftime("%a %e. %b %Y %R")
+    end
+
+    sprintf "%s - %s", from, to
   end
 
 end
