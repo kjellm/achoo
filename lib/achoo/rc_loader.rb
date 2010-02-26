f739600136e2e772a24d642e702d89f58d0dbc8e
Kjell-Magne Øierud <kjellm@acm.org>
2010-02-26 22:28:50 +0100

    Validate run control file content

--- lib/achoo/rc_loader.rb
+++ lib/achoo/rc_loader.rb
@@ -2,19 +2,54 @@ class Achoo; end
 
 module Achoo::RCLoader
 
-  def load_rc
-    rc_file = "#{ENV['HOME']}/.achoo"
+  RC_FILE = "#{ENV['HOME']}/.achoo"
 
-    # FIX test
-    if File.stat(rc_file).mode != 0100600
-      warn "Insecure permissions on #{rc_file}"
+  def load_rc
+    #create_empty_rc_if_not_exists
+    file_permissions_secure?
+
+    load RC_FILE
+
+    verify_rc_contents
+  end
+
+  private
+
+  def file_permissions_secure?
+    # FIX test to is to strict
+    if File.stat(RC_FILE).mode != 0100600
+      warn "Insecure permissions on #{RC_FILE}"
+      exit 1
+    end
+  end
+
+  def create_empty_rc_if_not_exists
+    return if FileTest.exist?(RC_FILE)
+
+    FileUtils.touch(RC_FILE)
+    FileUtils.chmod(0600, RC_FILE)
+  end
+
+  def verify_rc_contents
+    unless Object.const_defined?('RC')
+      warn "Malformed run control file: No RC constant defined"
       exit 1
     end
 
-    load rc_file
+    %w(url user password).each do |key|
+      unless RC.has_key?(key.to_sym)
+        warn "Missing mandatory run control configuration variable: #{key}"
+        exit 1
+      end
+    end
 
-    RC[:vcs_dirs] ||= []
-    RC[:ical]     ||= []
+    %w(vcs_dirs ical).each do |key|
+      unless RC.has_key?(key.to_sym)
+        warn "Missing run control configuration variable: #{key}. " \
+             + "Add it to #{RC_FILE} to get rid of this warning"
+        RC[key] = []
+      end
+    end
   end
 
 end
