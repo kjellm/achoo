f2e04fa9d8de3e978cc5af7ad5712d8816956916
Kjell-Magne Øierud <kjellm@acm.org>
2010-04-04 08:41:29 +0200

    Use shoulda for timespan unit tests

--- test/unit/test_timespan.rb
+++ test/unit/test_timespan.rb
@@ -1,29 +1,49 @@
 require 'achoo/timespan'
+require 'shoulda'
 require 'test/unit'
 
 class TestTimespan < Test::Unit::TestCase
-  # def setup
-  # end
-  
-  # def teardown
-  # end
 
-  def test_contains_with_timeish
-    ts = Achoo::Timespan.new('1970-01-01', '1970-01-02')
-    assert(ts.contains?('1970-01-01'))
-    assert(ts.contains?('1970-01-02'))
-    assert(ts.contains?('1970-01-01 23:30'))
+  context 'Contains with timeish argument' do
+    setup do
+      @ts = Achoo::Timespan.new('1970-01-01', '1970-01-02')
+    end
+    
+    should "contain it's start" do
+      assert(@ts.contains?('1970-01-01'))
+    end
 
-    assert(!ts.contains?('1970-01-03'))
+    should "contain it's end" do
+      assert(@ts.contains?('1970-01-02'))
+    end
+
+    should "contain an intervening time" do
+      assert(@ts.contains?('1970-01-01 23:30'))
+    end
+
+    should "not contain a time after end time" do
+      assert(!@ts.contains?('1970-01-03'))
+    end
   end
 
-  def test_contains_with_interval
-    ts = Achoo::Timespan.new('1970-01-01', '1970-01-02')
+  context 'Contains with timespan argument' do
+    setup do
+      @ts = Achoo::Timespan.new('1970-01-01', '1970-01-02')
+    end
 
-    assert(ts.contains?(Achoo::Timespan.new('1970-01-01', '1970-01-02')))
-    assert(ts.contains?(Achoo::Timespan.new('1970-01-01 06:00', '1970-01-01 09:00 ')))
+    should 'contain self' do
+      assert(@ts.contains?(@ts))
+    end
 
-    assert(!ts.contains?(Achoo::Timespan.new('1970-01-01 06:00', '1970-01-02 09:00 ')))
+    should 'contain intervening timespan' do
+      assert(@ts.contains?(Achoo::Timespan.new('1970-01-01 06:00', 
+                                               '1970-01-01 09:00 ')))
+    end
+
+    should 'not contain timespan with end after end time' do
+      assert(!@ts.contains?(Achoo::Timespan.new('1970-01-01 06:00',
+                                                '1970-01-02 09:00 ')))
+    end
   end
 
 end
