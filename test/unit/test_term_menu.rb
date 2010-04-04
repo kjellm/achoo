c3b1cf9180e3a4df99a6b83f206e607229cdcaf0
Kjell-Magne Øierud <kjellm@acm.org>
2010-04-04 11:11:33 +0200

    Use shoulda for term_menu unit tests

--- test/unit/test_term_menu.rb
+++ test/unit/test_term_menu.rb
@@ -1,6 +1,6 @@
 require 'achoo/term/menu'
 require 'stringio'
-require 'test/unit'
+require 'test_helpers'
 
 class Achoo
   class Term
@@ -17,37 +17,38 @@ class TestTermMenu < Test::Unit::TestCase
   # def teardown
   # end
 
-  def test_simple_case_should_work
-    menu    = Achoo::Term::Menu.new('', %w(a b c))
-    $stdout = StringIO.new
-    answer  = menu.print_ask_and_validate
-    expected_menu = <<'EOT'
+  context '#print_ask_and_validate' do
+    should 'simple case' do
+      menu    = Achoo::Term::Menu.new('', %w(a b c))
+      $stdout = StringIO.new
+      answer  = menu.print_ask_and_validate
+      expected_menu = <<'EOT'
  1. a
  2. b
  3. c
 EOT
-    res = $stdout.string
-    $stdout = STDOUT
-    assert_equal expected_menu, res
-    assert_equal "1", answer
-end
+      res = $stdout.string
+      $stdout = STDOUT
+      assert_equal expected_menu, res
+      assert_equal "1", answer
+    end
 
-  def test_empty_menu
-    menu    = Achoo::Term::Menu.new('', %w())
-    $stdout = StringIO.new
-    answer  = menu.print_ask_and_validate
-    expected_menu = ''
-    res = $stdout.string
-    $stdout = STDOUT
-    assert_equal expected_menu, res
-    assert answer.nil?
-  end
+    should 'empty menu' do
+      menu    = Achoo::Term::Menu.new('', %w())
+      $stdout = StringIO.new
+      answer  = menu.print_ask_and_validate
+      expected_menu = ''
+      res = $stdout.string
+      $stdout = STDOUT
+      assert_equal expected_menu, res
+      assert answer.nil?
+    end
 
-  def test_lots_of_options_right_justified_enumeration
-    menu    = Achoo::Term::Menu.new('', %w(a b c d e f g h i j k l), 'spec')
-    $stdout = StringIO.new
-    answer  = menu.print_ask_and_validate
-    expected_menu = <<'EOT'
+    should 'lots of options right justified_enumeration' do
+      menu    = Achoo::Term::Menu.new('', %w(a b c d e f g h i j k l), 'spec')
+      $stdout = StringIO.new
+      answer  = menu.print_ask_and_validate
+      expected_menu = <<'EOT'
   1. a
   2. b
   3. c
@@ -62,10 +63,12 @@ end
  12. l
   0. spec
 EOT
-    res = $stdout.string
-    $stdout = STDOUT
-    assert_equal expected_menu, res
-    assert_equal "1", answer
+      res = $stdout.string
+      $stdout = STDOUT
+      assert_equal expected_menu, res
+      assert_equal "1", answer
+    end
+
   end
 
 end
